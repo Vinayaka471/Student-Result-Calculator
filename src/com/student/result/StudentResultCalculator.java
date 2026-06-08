71d3413e2f561b211f1028a068746c57b7bbdabc
Vinayaka H <vinayakah47@gmail.com>
2026-06-08 11:06:58 +0530

    Implementing Student Percentage Calculator Project

--- src/com/student/result/StudentResultCalculator.java
+++ src/com/student/result/StudentResultCalculator.java
@@ -1,18 +1,32 @@
-package com.student.result;
+    package com.student.result;
 
-import java.util.Scanner;
+    import java.util.Scanner;
 
-public class StudentResultCalculator {
-    public static void main(String[] args){
-        System.out.println("Enter the Student Name: ");
-        Scanner sc=new Scanner(System.in);
-        String name=sc.nextLine();
-        System.out.println("Enter the Kannada Marks: ");
-        int Kannada=sc.nextInt();
-        System.out.println();
+    public class StudentResultCalculator {
+        public static void main(String[] args){
+            System.out.println("Enter the Student Name: ");
+            Scanner sc=new Scanner(System.in);
+            String name=sc.nextLine();
+            System.out.println("Enter the Kannada Marks: ");
+            int Kannada=sc.nextInt();
+            System.out.println("Enter the English Marks: ");
+            int English=sc.nextInt();
+            System.out.println("Enter the Hindi Marks: ");
+            int Hindi=sc.nextInt();
+            System.out.println("Enter the Maths Marks: ");
+            int Maths=sc.nextInt();
+            System.out.println("Enter the Science Marks: ");
+            int Science=sc.nextInt();
+            System.out.println("Enter the Social Science Marks");
+            int SocialScience=sc.nextInt();
+            int total= Kannada+English+Hindi+Maths+Science+SocialScience;
+            System.out.println("Total marks: "+total);
+            double percentage=(total/600.00)*100;
+            System.out.println("Total percentage of the students: "+percentage);
 
 
 
 
+
+        }
     }
-}
